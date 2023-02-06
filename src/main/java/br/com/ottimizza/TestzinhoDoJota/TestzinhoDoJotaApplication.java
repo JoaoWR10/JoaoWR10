6f6623fa96131d963eb3adb39fcfec53147177d9
Daniel Steil <dani.steil@hotmail.com>
2023-02-06 14:58:14 -0300

    testando o commit

---
+++ src/main/java/br/com/ottimizza/TestzinhoDoJota/TestzinhoDoJotaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ottimizza.TestzinhoDoJota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestzinhoDoJotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestzinhoDoJotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ottimizza/TestzinhoDoJota/TestzinhoDoJotaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ottimizza.TestzinhoDoJota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestzinhoDoJotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
